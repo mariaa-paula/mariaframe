f8a91690a33445faa6347859509ca73ff265e403
mariaa-paula <maria.freitas5@estudante.ifms.edu.br>
2026-04-08 11:45:16 -0400

    quase funcionando

--- backend/src/todos/todos.module.ts
+++ backend/src/todos/todos.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { PrismaService } from 'src/shared/databases/prisma.databases';
+import * as UseCase from './use-cases';
+import * as Repositories from './repository';
+
+const useCase = Object.values(UseCase);
+const repositories = Object.values(Repositories);
 
 @Module({
   controllers: [TodosController],
-  providers: [TodosService],
+  providers: [
+    TodosService,
+    PrismaService,
+    Logger,
+    ...repositories,
+    ...useCase
+  ],
 })
 export class TodosModule {}
